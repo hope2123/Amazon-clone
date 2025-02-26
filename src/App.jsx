16ef68a0727b8065f8a54035c8211decc05f7dd1
hope14657262 <hopemelese89@gmail.com>
2025-02-26 20:44:18 +0300

    add logo amazon and headr file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
+import Header from './Componet/Header/Header'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
    <h1>for test amazone -clone website</h1>
-        
+   <Header/>
     </>
   )
 }
