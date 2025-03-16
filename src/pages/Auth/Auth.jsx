96ceeb21ba7f5d2e09bdff3389939db4af74995a
hope14657262 <hopemelese89@gmail.com>
2025-03-16 22:21:30 +0300

    set user and funcationalityauntfication

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,24 +1,54 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import  './Auth.css'
 import Layout from '../../Componet/Layout/Layout'
 import { Link } from 'react-router'
 import amazon_letter_logo from "../../assets/image/logo/amazon_letter_logo.png"
 import { auth } from '../../Utility/firebase'
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
+import {signInWithEmailAndPassword,createUserWithEmailAndPassword,} from "firebase/auth";
+import {DataContext} from '../../Componet/DataProvider/DataProvider'
+import {Type} from '../../Utility/action.type'
+
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  const [{ user }, dispatch] = useContext(DataContext);
+    console.log(user)
+
   //console.log(password, email)
-  const authHandler = async (e) => {
+  const authHandler =  async(e) => {
     e.preventDefault();
 
     // console.log(e);
     // console.log(e.target);
     // console.log(e.target.name);
+  
+
+if (e.target.name == "signIn") {signInWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+           //console.log(userInfo);
+           dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+          })
+          .catch((err) => {
+           //  console.log(err);
+             setError(err.message);
+          });
+} else{createUserWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+         //  console.log(userInfo);
+         dispatch({
+          type: Type.SET_USER,
+          user: userInfo.user,
+        });
+}).catch((err) => {
+  //console.log(err);
+  setError(err.message);
+});
+}
   }
   return (
     <Layout>
@@ -30,23 +60,29 @@ function Auth() {
       <div className="auth__signin_container">
         <h1>Sign In</h1>
         <form action="">
-          <div>
-            <label htmlFor="email">Email</label>
-            <input value={email}
-              onChange={(e) => setEmail(e.target.value)} type="email"
-              id="email" />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input value={password}
-              onChange={(e) => setPassword(e.target.value)} type="password"
-              id="password"/>
-          </div>
-
+  <div>
+    <label htmlFor="email">Email</label>
+    <input value={email}
+              onChange={(e) => setEmail(e.target.value)}
+      id="email"
+      type="email"
+      autoComplete="username" // For login forms
+    />
+  </div>
+  <div>
+    <label htmlFor="password">Password</label>
+    <input value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+      id="password"
+      type="password"
+      autoComplete="current-password" // For existing passwords
+    />
+  </div>
       
         <button type="submit"
             onClick={authHandler}
-            name="signIn"className='auth__signin_signInButton'  >Sign in</button>
+            name="signIn"
+            className='auth__signin_signInButton' >Sign in</button>
       
         </form>
         <p>
@@ -60,7 +96,7 @@ function Auth() {
           onClick={authHandler}
         className='auth_login__registerButton'>Create your Amazon Account</button>
       </div>
-            
+      <div>{error}</div>
     </section>
     </Layout>
   )
