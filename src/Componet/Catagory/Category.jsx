dc2971eb5509d2e632cd375c4d2f69672458ee68
hope14657262 <hopemelese89@gmail.com>
2025-03-26 01:32:20 +0300

    amazon fronted deployment

--- src/Componet/Catagory/Category.jsx
+++ src/Componet/Catagory/Category.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './category.css'
+import './Category.css';
 import {CategoryData} from './CategoryData'
 import CategoryCard from './CategoryCard'
 function Category() {
